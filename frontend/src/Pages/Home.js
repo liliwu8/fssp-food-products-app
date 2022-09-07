5060a96ee39e03d7a1fff9b6f287456d37fa55c0
lily08 <liliangelxd@gmail.com>
2022-09-06 23:47:28 -0400

    delete stuff

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,19 +1,4 @@
-// import world from './World Cusine.png'
-
-import axios from 'axios'
-import { useEffect, useState} from 'react'
-const API = process.env.REACT_APP_API_URL
-// import giphy from './giphy.gif'
 function Home() {
-  const [food, setFood] = useState([])
-
-  useEffect(() => {
-    axios.get(`${API}/foods`).then((res) => {
-      setFood(res.data.payload)
-    })
-  }, [])
-
-  
   return (
     <div>
       <h2 className='animatecss animatecss-flipInX animatecss-delay-1s  text-3xl text-[#4D3B2A] font-bold uppercase mb-[2%] mt-[3%]'>
@@ -21,8 +6,6 @@ function Home() {
       </h2>
 
       {/* <img src={giphy} className='background-color-blue-500' /> */}
-
-      
     </div>
   )
 }
